b14cc2e067690c598f2531682b7a2869e9c80951
tadosuke <tadosuke@gmail.com>
2025-07-09 05:44:38 +0900

    フォルダ構成を整理

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 静的ファイルの配信ディレクトリ
-const staticDir = path.resolve(__dirname, "../dist");
+const staticDir = path.resolve(__dirname, "../dist-src");
 
 app.use(express.static(staticDir));
 
