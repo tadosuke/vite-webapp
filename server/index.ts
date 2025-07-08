258bcba168d92868c8f3a06528cd8295de7accdb
tadosuke <tadosuke@gmail.com>
2025-07-09 06:06:18 +0900

    静的ファイルの配信ディレクトリの解決方法を修正

--- server/index.ts
+++ server/index.ts
@@ -3,14 +3,13 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import routes from './routes.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 静的ファイルの配信ディレクトリ
-const staticDir = path.resolve(__dirname, '../dist-src');
+const staticDir = path.resolve(
+  fileURLToPath(new URL('../dist-src', import.meta.url)),
+);
 
 app.use(express.static(staticDir));
 app.use(routes);
