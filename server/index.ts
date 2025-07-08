39eaa4edf20646fae25a234d4e27a10eabcd778c
tadosuke <tadosuke@gmail.com>
2025-07-09 06:31:10 +0900

    server: 修正された静的ファイルの配信ディレクトリの設定を簡素化

--- server/index.ts
+++ server/index.ts
@@ -3,14 +3,12 @@ import path from 'path';
 import routes from './routes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// 静的ファイルの配信ディレクトリ
-const staticDir = path.resolve(process.cwd(), 'dist-src');
-
-app.use(express.static(staticDir));
+app.use(express.static(path.resolve(process.cwd(), 'dist-src')));
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server is running at http://localhost:${process.env.PORT || 3000}`,
+  );
 });
